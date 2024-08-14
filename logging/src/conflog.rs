8e1004bad5d609facfb73123a63cff96b7154a9a
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2024-08-14 18:55:32 +0200

    wip

--- logging/src/conflog.rs
+++ logging/src/conflog.rs
@@ -1,10 +1,10 @@
 use colored::*;
 use env_logger;
-use std::fs::OpenOptions;
+// use std::fs::OpenOptions;
 use std::io::Write;
+use flexi_logger::{Age, Cleanup, Criterion, FileSpec, Logger, Naming};
 
 pub fn init() {
-    let var_name = "LOG_LEVEL";
     let mut builder = env_logger::Builder::new();
     builder
         // Формирование сообщения здесь
@@ -32,35 +32,42 @@ pub fn init() {
                     record.file().unwrap_or("unknown"),
                     record.line().unwrap_or(0)
                 )
-                .blue(),
+                    .blue(),
                 chrono::Local::now()
                     .format("%Y-%m-%dT%H:%M:%S")
                     .to_string()
                     .dimmed(),
             )
         })
-        .parse_env(var_name);
+        .parse_env("LOG_LEVEL");
 
-    if let Ok(value) = std::env::var("LOG_FILE_PATH") {
-        let log_file = OpenOptions::new()
-            .create(true)
-            .write(true)
-            .append(true)
-            .open(value)
-            .expect("Failed to open log file");
+    let log_level = std::env::var("LOG_LEVEL")
+        .unwrap_or("info".to_string());
 
-        builder
-            .target(env_logger::Target::Pipe(Box::new(log_file)))
-            .init();
+    if let Ok(value) = std::env::var("LOG_DIRECTORY_PATH") {
+        println!("LOG_FILE_PATH={:?}", value);
+        println!("LOG_LEVEL={:?}", log_level);
+        Logger::try_with_str(log_level.clone())
+            .unwrap()
+            .log_to_file(
+                flexi_logger::FileSpec::default().directory("logs"),
+            )
+            .rotate(
+                flexi_logger::Criterion::Age(Age::Day),
+                flexi_logger::Naming::Numbers,
+                flexi_logger::Cleanup::KeepLogFiles(7),
+            )
+            .start()
+            .unwrap();
     } else {
         builder.init();
     }
 
     log::info!(
         "{}={}",
-        var_name.blue(),
-        std::env::var(var_name)
-            .unwrap_or("<Перемпенная не определена.>".to_string())
+        log_level.clone().blue(),
+        std::env::var("LOG_LEVEL")
+            .unwrap_or("<Переменная LOG_LEVEL не определена. по умолчанию 'info'>".to_string())
             .green(),
     );
 }
