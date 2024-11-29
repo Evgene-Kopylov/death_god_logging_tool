a1c3ed48a4d45a071cada05b91e146b838a1ab92
Evgene-Kopylov <evgenekopylov@gmail.com>
2024-11-29 15:11:49 +0200

    выключил панику на всякий случай

--- logging/src/main.rs
+++ logging/src/main.rs
@@ -6,11 +6,13 @@ fn main() {
 
     // let log_path = "logs";
     // conflog::init(log_level.to_string(), Some(log_path.to_string()));
-    conflog::init("trace".to_string(), None).ok();
+    conflog::init("trace".to_string(), Some("logs".to_string())).ok();
 
     log::trace!("ttt - записть в лог-файл");
     log::debug!("ddd - записть в лог-файл");
     log::info!("i - записть в лог-файл");
     log::warn!("w - записть в лог-файл");
     log::error!("eee - записть в лог-файл");
+    // println!("print line ...");
+    // panic!("Паника!!!");
 }
