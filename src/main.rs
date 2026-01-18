1781471f257ac3c9756082fdeec0f2c682fe4d6c
death god <evgenekopylov@gmail.com>
2026-01-18 22:26:06 +0200

    ..

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use death_god_logging_tool::conflog;
 
 fn main() {
     println!("1 - вывод до инициализации логгера");
-    conflog::init("trace".to_string(), Some("logs".to_string()), true).ok();
+    // conflog::init("trace".to_string(), Some("logs".to_string()), true).ok();
+    conflog::init("trace".to_string(),None, true).ok();
     println!("2 - вывод после инициализации логгера");
 
     log::trace!("ttt - запись в лог-файл");
